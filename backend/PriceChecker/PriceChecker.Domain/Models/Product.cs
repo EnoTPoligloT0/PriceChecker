1e57d0d8e99c13cf282db3c10f7fee79efbf8d29
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-07 23:43:02 +0100

    Product model

---
+++ backend/PriceChecker/PriceChecker.Domain/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace PriceChecker.Domain.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Url { get; set; }
+    public string Website { get; set; }
+}
