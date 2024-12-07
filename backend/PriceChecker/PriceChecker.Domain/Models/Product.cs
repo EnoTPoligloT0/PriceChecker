d2ce0f1ba0ad9bdf80a278b4ffc16e8e82cffd71
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-08 00:49:02 +0100

    Variables refactoring

--- backend/PriceChecker/PriceChecker.Domain/Models/Product.cs
+++ backend/PriceChecker/PriceChecker.Domain/Models/Product.cs
@@ -2,8 +2,8 @@ namespace PriceChecker.Domain.Models;
 
 public class Product
 {
-    public string Name { get; set; }
+    public string ProductName { get; set; }
     public decimal Price { get; set; }
     public string Url { get; set; }
-    public string Website { get; set; }
+    public string SiteName { get; set; }
 }
