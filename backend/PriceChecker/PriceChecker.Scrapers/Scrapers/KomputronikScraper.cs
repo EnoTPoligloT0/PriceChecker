3ee9d0552a47e777d1800a13dc244a1d0ae2e3ab
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-12-09 23:29:29 +0100

    Komputronik scraper implementation

---
+++ backend/PriceChecker/PriceChecker.Scrapers/Scrapers/KomputronikScraper.cs
@@ -0,0 +1,43 @@
+using HtmlAgilityPack;
+using PriceChecker.Domain.Interfaces;
+using PriceChecker.Domain.Models;
+
+namespace PriceChecker.Scrapers.Scrapers;
+
+public class KomputronikScraper(HttpClient httpClient) : BaseScraper(httpClient), ISiteScraper
+{
+    public string SiteName => "Komputronik";
+
+    public override string GetSearchUrl(string searchTerm)
+    {
+        return $"https://www.komputronik.pl/search/category/1?query={Uri.EscapeDataString(searchTerm)}";
+    }
+
+    public async Task<List<Product>> ScrapeProductsAsync(string searchTerm)
+    {
+        var searchUrl = GetSearchUrl(searchTerm);
+        var html = await FetchHtmlAsync(searchUrl, "https://www.komputronik.pl");
+        await File.WriteAllTextAsync("debugKomputronik.html", html );
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(html);
+        
+        var productNodes = htmlDoc.DocumentNode.SelectNodes(
+            "//div[@data-name='listingTile']");
+
+        Console.WriteLine($"Found {productNodes.Count} products in Komputronik");
+        if (productNodes == null || !productNodes.Any())
+        {
+            Console.WriteLine("No product nodes found on the Komputronik.");
+            return new List<Product>(); 
+        }
+        return ParseProductNodes(
+            productNodes,
+            SiteName,
+            "https://www.komputronik.pl",
+            ".//h2[@class='line-clamp-3 font-headline text-lg font-bold leading-6 md:text-xl md:leading-8']",
+            ".//div[@data-name='listingPrice']//div[@class='flex-col mt-2 inline-flex flex-wrap']//div[@data-price-type='final']/text()",
+            ".//div[@class='md:col-span-2']//a[@href]/@href",
+            false
+        );
+    }
+}
